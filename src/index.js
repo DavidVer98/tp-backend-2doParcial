1546b5e05070863ac2434f3f64d5afb89a7d07df
DavidVer98 <davidcuevas15@hotmail.com>
2023-05-12 18:53:39 -0400

    Add automatic import module

--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
 import app from './app.js';
-import {sequelize} from './database/database.js';
-// import './models/cateogoria.model.js';
+import { sequelize } from './database/database.js';
+import * as models from './models/index.js';
 
 async function main() {
-    try{
-        await sequelize.sync({force: false});
+    try {
+        await sequelize.sync({ force: false });
         console.log('Connection has been established successfully.');
-    }
-    catch(error){
+
+        // Associate all models
+        Object.values(models).forEach((model) => {
+            if (model.associate) {
+                model.associate(models);
+            }
+        });
+    } catch (error) {
         console.error('Unable to connect to the database:', error);
     }
     await app.listen(3000, () => {
-        // console.log('Config', sequelize.config);
+           // console.log('Config', sequelize.config);
         console.log('Server is running on port 3000');
     });
 }
