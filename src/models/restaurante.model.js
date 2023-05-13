960c247775011fdef3654de367762f6c0b4a62b8
DavidVer98 <davidcuevas15@hotmail.com>
2023-05-12 20:08:16 -0400

    add restaurante

---
+++ src/models/restaurante.model.js
@@ -0,0 +1,16 @@
+import { DataTypes } from 'sequelize';
+import { sequelize } from '../database/database.js';
+
+export const Restaurante = sequelize.define('restaurante', {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  nombre: {
+    type: DataTypes.TEXT,
+  },
+  direccion: {
+    type: DataTypes.TEXT,
+  },
+});
